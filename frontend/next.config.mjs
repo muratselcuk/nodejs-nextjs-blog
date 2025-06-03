3871fec1206e77c4dcdb0a5a86565f93d2e3cd97
Murat <muratsel@gmail.com>
2025-06-03 04:51:27 +0300

    undo rewrite changing

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV !== 'production';
-
 const nextConfig = {
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
+  },  
   async rewrites() {
-    if (isDev) {
-      return [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+      },
+    ];
   },
 };
 
